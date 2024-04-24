c9bde49501c7bfb85dad8fadfc493d55dcf4f36d
CrozzDev <crozzdev95@outlook.com>
2024-04-24 15:13:34 -0500

    Started pokemon app functionality

---
+++ projects/poke_app_oop/script.js
@@ -0,0 +1,17 @@
+const $searchInput = document.getElementById("search-input");
+const $searchButton = document.getElementById("search-button");
+
+const $pokemonNameSpan = document.getElementById("pokemon-name");
+const $pokemonIdSpan = document.getElementById("pokemon-id");
+const $pokemonWeightSpan = document.getElementById("weight");
+const $pokemonHeightSpan = document.getElementById("height");
+const $pokemonImgDiv = document.getElementById("pokemon-img");
+
+// stats
+
+const $hpDiv = document.getElementById("hp");
+const $attackDiv = document.getElementById("attack");
+const $defenseDiv = document.getElementById("defense");
+const $spAttackDiv = document.getElementById("special-attack");
+const $spDefenseDiv = document.getElementById("special-defense");
+const $speedDiv = document.getElementById("speed");
